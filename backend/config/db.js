75df061d45ff1db46b0394505ccbc64e88c2fac9
softwareprymaerospace@gmail.com <softwareprymaerospace@gmail.com>
2025-06-27 15:33:22 +0530

    added some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Invoice database connected successfully"))
     .catch((err) => console.log("the error in addding database is : ", err))
 

fe3e5aab0f4637d67021f5b3433c062d5b28acd8
softwareprymaerospace@gmail.com <softwareprymaerospace@gmail.com>
2025-06-27 15:47:47 +0530

    added some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 console.log('connectinig to mongo db', process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://softwareprymaerospace:xPb0CVMljp2lnsdP@invoice-cluster.zmcpuhe.mongodb.net/invoiceGenerator?retryWrites=true&w=majority&appName=invoice-cluster")
     .then(() => console.log("Invoice database connected successfully"))
     .catch((err) => console.log("the error in addding database is : ", err))
 

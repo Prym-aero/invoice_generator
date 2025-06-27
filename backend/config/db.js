923259f7ad639c7aaec860f3f6a88ad8a9916491
softwareprymaerospace@gmail.com <softwareprymaerospace@gmail.com>
2025-06-27 15:42:47 +0530

    added som changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose')
 require('dotenv').config();
+const mongoose = require('mongoose')
+
+console.log('connectinig to mongo db', process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Invoice database connected successfully"))
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174']
+    origin: ['https://invoice-generator-lyart.vercel.app', 'http://localhost:5174']
 }))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api', FileRoute);
-app.use('/api',budgetRoute)
+app.use('/api', budgetRoute)
 app.use('/api', invoiceRoute);
 
 
